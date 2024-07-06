bef738172372762810ca7788e08d586867caa5e3
ahmedferganey <ahmed.ferganey707@gmail.com>
2024-07-06 21:14:10 +0300

    DataTypes

--- CPP_Learning/main.cpp
+++ CPP_Learning/main.cpp
@@ -5,21 +5,23 @@ int main()
 {
   int x = 5;
   int y = 10;
+  int z = {10};
+  const float pi = 3.14;
+
+  bool myBoolean = true;       // Boolean (true or false)
 
 
-  
-  
   std::cout << "Hello World!" << std::endl;
-  
+  cout << "i am learning c++ !" << "\n\n";
   cout << "Hello World! \n";
-
-
-  std::cout << x * y << std::endl;
-  
-  
+  std::cout << x * y * z<< std::endl;
   cout << "Hello World! \n";
+  cout << pi << "\n";
 
-  
+  int myAge = 35;
+  cout << "I am " << myAge << " years old.";
+
+
   }
 
 
